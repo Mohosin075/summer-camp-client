3cf6c51b3911a8082e2a7549953e341183c2afb6
Md Alim <mdalimoattabur@gmail.com>
2023-06-14 04:24:28 +0600

    delete method added for my select class

--- src/dashBoard/students/selectedClass/SelectedClass.jsx
+++ src/dashBoard/students/selectedClass/SelectedClass.jsx
@@ -2,6 +2,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../../providers/AuthProvider';
 import SetPageTitle from '../../../components/setPageTitle';
+import Swal from 'sweetalert2';
 const SelectedClass = () => {
     const {user}  = useContext(AuthContext)
     const [selectedItems, setSelectedItems] = useState([])
@@ -15,7 +16,33 @@ const SelectedClass = () => {
 
 
     const handleRemove =(id)=>{
-        console.log("clicked", id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/selectItemDelete/${id}`, {
+                    method : 'DELETE'
+                })
+                .then(res=>res.json())
+                .then(data=>{
+                    if(data.deletedCount > 0){
+                        window.location.reload()
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                          )
+                    }
+                })
+            }
+          })
     }
 
     return (
